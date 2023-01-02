ef0e5aa24f10b1f6d718f05cec792d6243e3ea41
Seiyial Aurum <sayhao@seiyial.xyz>
2023-01-02 15:04:52 +0800

    Add autoinstall eslint packages

--- install.ts
+++ install.ts
@@ -1,13 +1,21 @@
 // installer file for eslintrc
-import { installer } from '../../src/lib/deploy/installer'
+import { ShardInstallerPack } from '../../../../src/lib/deploy/ShardInstallerPack'
 
 const install = async (
-	installer: installer.Pack
+	installer: ShardInstallerPack
 ): Promise<{ ok: boolean, error?: string }> => {
 	
 	// this is required somewhere in the install process. if not called, it will be called after.
 	const coreInstall = await installer.performCoreInstall()
 	if (!coreInstall.ok) return coreInstall
+
+	installer.addDevNPMPackages([
+		'@typescript-eslint/parser',
+		'@typescript-eslint/eslint-plugin',
+		'eslint',
+		'eslint-plugin-react',
+		'eslint-plugin-react-hooks'
+	])
 
 	return { ok: true }
 
